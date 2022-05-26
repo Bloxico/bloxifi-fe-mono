a2b1a9f5b5a733be56b0f7e305b858db1f8b6860
Kristina <kristinapejcic1994@gmail.com>
2022-05-26 15:02:00 +0200

    feat: Detect wrong network connection on Metamask [BLOX-97]

--- apps/web/src/components/WalletConnection/ConnectWalletButton.tsx
+++ apps/web/src/components/WalletConnection/ConnectWalletButton.tsx
@@ -1,12 +1,34 @@
 /* eslint-disable */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { BoxLayout, Text, StackLayout } from '@bloxifi/ui'
 
+const supportedChains = {
+  1: 'mainnet',
+  3: 'ropsten',
+  4: 'rinkeby',
+  42: 'kovan',
+  1284: 'moonbeam',
+}
+
+declare global {
+  interface Window {
+    ethereum?: any
+  }
+}
+
 export const ConnectWalletButton = () => {
   const [hasError, setHasError] = useState(false)
+  const [currentNetwork, setCurrentNetwork] = useState<number | undefined>()
   const web3Context = useWeb3React()
+  const isWrongNetworkConnected =
+    currentNetwork && !supportedChains[currentNetwork]
+  const isMetamaskInstalled = typeof window.ethereum !== 'undefined'
+
+  useEffect(() => {
+    web3Context.chainId && setCurrentNetwork(web3Context.chainId)
+  }, [web3Context.chainId])
 
   async function connect() {
     try {
@@ -25,6 +47,14 @@ export const ConnectWalletButton = () => {
     }
   }
 
+  if (!isMetamaskInstalled) {
+    return (
+      <Text type="text xl" color="red" semiBold align="center">
+        Please install Metamask
+      </Text>
+    )
+  }
+
   if (hasError) {
     return (
       <Text type="text xl" color="red" semiBold align="center">
@@ -35,21 +65,31 @@ export const ConnectWalletButton = () => {
 
   return (
     <BoxLayout>
-      {web3Context.active ? (
-        <StackLayout>
-          <Text type="text xl" color="green" semiBold align="center">
-            Connected!
-          </Text>
+      <StackLayout>
+        {web3Context.active ? (
+          <>
+            {isWrongNetworkConnected ? (
+              <Text type="text xl" color="red" semiBold align="center">
+                Wrong network connection!
+              </Text>
+            ) : (
+              <>
+                <Text type="text xl" color="green" semiBold align="center">
+                  {`Connected to ${supportedChains[currentNetwork]} !`}
+                </Text>
+                <Text type="text xl" color="textGray" semiBold align="center">
+                  {web3Context.account}
+                </Text>
+                <button onClick={disconnect}>Disconnect</button>
+              </>
+            )}
+          </>
+        ) : (
           <Text type="text xl" color="textGray" semiBold align="center">
-            {web3Context.account}
+            <button onClick={connect}>Connect wallet</button>
           </Text>
-          <button onClick={disconnect}>Disconnect</button>
-        </StackLayout>
-      ) : (
-        <Text type="text xl" color="textGray" semiBold align="center">
-          <button onClick={connect}>Connect wallet</button>
-        </Text>
-      )}
+        )}
+      </StackLayout>
     </BoxLayout>
   )
 }
